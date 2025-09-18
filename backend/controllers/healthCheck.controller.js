55fde77ab32035e66a57f24593d4819ff7b5d299
om shukla <omshukla@oms-MacBook-Air.local>
2025-09-18 20:03:53 +0530

    feature/healthcheck

--- backend/controllers/healthCheck.controller.js
+++ backend/controllers/healthCheck.controller.js
@@ -2,7 +2,7 @@ import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthCheck = asyncHandler(async (req,res)=>{
-  return res.status(200).json(new apiResponse(true,200, "API is working fine health check has been passed", null, null))
+  return res.status(200).json(new apiResponse(200, "OK", "Health check passed"));
 })
 
 export { healthCheck }
